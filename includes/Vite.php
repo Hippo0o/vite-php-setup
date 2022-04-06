5e6c3c32612200bd9a91f93bad5115ba13fae79f
Tobias S <tobias.seifert97@gmail.com>
2022-04-06 12:30:42 +0200

    more advanced fallback logic with timeout to circumvent long loading times on windows

--- includes/Vite.php
+++ includes/Vite.php
@@ -9,14 +9,14 @@ class Vite
     public string $viteServer;
 
     public function __construct(
-      string $entry,
-      bool $isDev = true,
-      string $publicPath = __DIR__ . '/dist/',
-      string $publicUrl = '/dist/',
-      string $viteServer = 'http://localhost:3000/'
+        string $entry,
+        bool $isDev = true,
+        string $publicPath = __DIR__ . '/dist/',
+        string $publicUrl = '/dist/',
+        string $viteServer = 'http://localhost:3000/'
     ) {
         $this->entry = $entry;
-        $this->isDev = $isDev; // should be set depending on the PHP environment to skip the possibly unecessary fallback logic
+        $this->isDev = $isDev;
         $this->publicPath = $publicPath; // basically the filesystem dir the manifest.json is written to
         $this->publicUrl = $publicUrl;
         $this->viteServer = $viteServer;
@@ -58,30 +58,33 @@ class Vite
 
     /*
      * The server running PHP might not have direct access to localhost, so the check is handled client-side.
-     * Checks for the vite dev server being available and loads built assets as fallback.
+     * Checks for the vite dev server being available and loads built assets if necessary.
      */
     protected function devEnv(): string
     {
-        $hash = substr(sha1($this->viteServer), -10);
+        $hash = substr(sha1(uniqid($this->viteServer)), -6);
         return "
           <script id='vite-dev-$hash' type='module' crossorigin src='{$this->viteServer}{$this->entry}'></script>
-          <template id='vite-fallback-$hash'>
+          <template id='vite-build-$hash'>
             {$this->jsTag()}
             {$this->jsPreloadImports()}
             {$this->cssTag()}
           </template>
           <script id='vite-devcheck-$hash'>
-            const fallback = document.getElementById('vite-fallback-$hash');
-            fetch('{$this->viteServer}{$this->entry}')
+            const template_$hash = document.getElementById('vite-build-$hash');
+            const controller_$hash = new AbortController();
+            const timeoutId_$hash = setTimeout(() => controller_$hash.abort(), 1000);
+            fetch('{$this->viteServer}{$this->entry}', { signal: controller_$hash.signal })
               .then((res) => {
                 if(res.status === 404) {
                     throw null;
                 }
-                fallback.remove();
+                clearTimeout(timeoutId_$hash);
+                template_$hash.remove();
               })
               .catch(() => {
-                document.getElementById('vite-dev-$hash').append(document.importNode(fallback.content, true));
-                fallback.remove();
+                document.getElementById('vite-dev-$hash').append(document.importNode(template_$hash.content, true));
+                template_$hash.remove();
               });
             document.getElementById('vite-devcheck-$hash').remove();
           </script>
@@ -101,8 +104,8 @@ class Vite
         $manifest = $this->getManifest();
 
         return isset($manifest[$this->entry])
-          ? $this->publicUrl . $manifest[$this->entry]['file']
-          : '';
+            ? $this->publicUrl . $manifest[$this->entry]['file']
+            : '';
     }
 
     public function importsUrls(): array
@@ -131,3 +134,4 @@ class Vite
         return $urls;
     }
 }
+
--- vite/vite.config.js
+++ vite/vite.config.js
@@ -8,7 +8,8 @@
 // IMPORTANT image urls in CSS works fine
 // BUT you need to create a symlink on dev server to map this folder during dev:
 // ln -s {path_to_vite}/src/assets {path_to_public_html}/assets
-// or import assets manually e.g. import logoUrl from '../assets/logo.png'
+// OR import assets manually e.g. import logoUrl from '../assets/logo.png'
+// this way they consider the server.origin config to generate absolute urls
 // on production everything will work just fine
 
 import { defineConfig } from 'vite';
@@ -20,61 +21,62 @@ const PORT = 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    liveReload([
-      // edit live reload paths according to your source code
-      // for example:
-      // __dirname + '/(app|config|views)/**/*.php',
-      // using this for our example:
-      __dirname + '/../public/*.php',
-    ]),
-  ],
+    plugins: [
+        vue(),
+        liveReload([
+            // edit live reload paths according to your source code
+            // for example:
+            // __dirname + '/(app|config|views)/**/*.php',
+            // using this for our example:
+            // __dirname + '/../public/*.php',
+        ]),
+    ],
 
-  // config
-  root: 'src',
-  base: '/',
+    // config
+    root: 'src',
+    base: '/',
 
-  build: {
-    // output dir for production build
-    outDir: path.resolve(__dirname, '../public/dist'),
-    emptyOutDir: true,
+    build: {
+        // output dir for production build
+        outDir: path.resolve(__dirname, '../public/dist'),
+        emptyOutDir: true,
 
-    // emit manifest so PHP can find the hashed files
-    manifest: true,
+        // emit manifest so PHP can find the hashed files
+        manifest: true,
 
-    // our entry
-    rollupOptions: {
-      input: path.resolve(__dirname, './src/main.js'),
+        // our entry
+        rollupOptions: {
+            input: path.resolve(__dirname, './src/main.js'),
+        },
     },
-  },
 
-  server: {
-    // required to load scripts from custom host
-    cors: true,
+    server: {
+        // required to load scripts from custom host
+        cors: true,
 
-    // required to get absolute urls for imported assets
-    origin: `http://localhost:${PORT}`,
-    base: '',
+        // required to get absolute urls for imported assets
+        origin: `http://localhost:${PORT}`,
+        base: '',
 
-    // we need a strict port to match on PHP side
-    // change freely, but update on PHP to match the same port
-    strictPort: true,
-    port: PORT,
+        // we need a strict port to match on PHP side
+        // change freely, but update on PHP to match the same port
+        strictPort: true,
+        port: PORT,
 
-    // always connect websocket to localhost
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws',
+        // always connect websocket to localhost
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+        },
     },
-  },
 
-  // required for in-browser template compilation
-  // https://v3.vuejs.org/guide/installation.html#with-a-bundler
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.esm-bundler.js',
+    // required for in-browser template compilation
+    // https://v3.vuejs.org/guide/installation.html#with-a-bundler
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+        extensions: ['.vue', '.js'],
     },
-    extensions: ['.vue', '.js'],
-  },
 });
+
